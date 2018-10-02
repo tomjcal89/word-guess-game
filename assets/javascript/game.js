e52425dcb3a46d2743c03d8ec90628b08321ea07
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-01 21:30:24 -0700

    added changes to javascrip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
+
 // list of the words that will be guessed
 
-var bowlingTerms = ["bumpers", "strike", "turkey", "spare", "gutter ball", "split", "bowling ball", 
-                    "bowling lane", "perfect game", "bowling pins"]
-
+var bowlingTerms = ["bumpers", "strike", "turkey", "spare", "gutter ball", "split", "bowling ball", "bowling lane", "perfect game", "bowling pins"];
 console.log(bowlingTerms)
+
+// scoring and turns elements
+
+var score = 0;
+var guessesRemaining = 15;
