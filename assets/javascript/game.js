cf2ff89badbabc4d9704750136ea5c2e74d64959
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-01 22:38:35 -0700

    additions to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 
-// list of the words that will be guessed
+// list of the words that will be guessed and availible letters
 
 var bowlingTerms = ["bumpers", "strike", "turkey", "spare", "gutter ball", "split", "bowling ball", "bowling lane", "perfect game", "bowling pins"];
-console.log(bowlingTerms)
 
 // scoring and turns elements
-
 var score = 0;
-var guessesRemaining = 15;
+var remainGuess = 11;
+
+var rand = bowlingTerms[Math.floor(Math.random() * bowlingTerms.length)];
