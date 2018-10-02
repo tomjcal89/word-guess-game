83fe9df80dc3149faee15e64d25e0400a671be9a
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-01 21:21:44 -0700

    linked css and javascript to html. started coding javascript- made array of words for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// list of the words that will be guessed
+
+var bowlingTerms = ["bumpers", "strike", "turkey", "spare", "gutter ball", "split", "bowling ball", 
+                    "bowling lane", "perfect game", "bowling pins"]
+
+console.log(bowlingTerms)
